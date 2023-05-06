ab5617a53ca6513ec16711bb373ec44c517f099d
Antonio <antonioterrazaslara@hotmail.com>
2023-05-05 20:53:30 -0500

    Removed DFU and cross variables from Makefile

--- micropy/ports/xilinx/mymake.sh
+++ micropy/ports/xilinx/mymake.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
-echo PATH env:
-echo $PATH
-echo  
 
-echo Python Home:
+echo Removing custom PYTHONHOME and PYTHONPATH from Vitis IDE
 unset PYTHONHOME
-echo $PYTHONHOME
-echo  
-echo Python Path:
 unset PYTHONPATH
-echo $PYTHONPATH
-echo  
-echo Python sys:
-echo $sys
-
-#export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin
-#echo $PATH
 
 echo removing build directory:
 rm -R build
 
 echo Starting Make:
-make V=1 CROSS=1
+make V=1
 cp build/micropython.elf ../../Debug/micropy.elf
 
